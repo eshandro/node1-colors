27d6e8259d1550a1962ed0cc7bf5269913fce4c7
Erik Shandrowski <erik843@gmail.com>
2014-07-22 11:56:05 -0600

    Finished Bonus 2

--- part3-dir/web-colors.js
+++ part3-dir/web-colors.js
@@ -1,16 +1,18 @@
 var request = require('request');
 var _ = require('underscore');
-var colorname = process.argv[2];
+var colorName = process.argv[2].toLowerCase();
 
-var findRGB = function() {
-	var colorname = process.argv[2];
-	var foundColor = _.find(
-	request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function(error, response, body) {
-		console.log(JSON.parse(body));
-	}),
-	function(item) {
-		return item.name === colorname;
+request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', 
+	function(error, response, body) {
+		var pageJSON = JSON.parse(body);
+
+		var colorItem = _.find(pageJSON, function(item) {
+			return item.name.toLowerCase() === colorName 
+		});
+		
+		var red = colorItem.rgb.r;
+		var green = colorItem.rgb.g;
+		var blue = colorItem.rgb.b;
+		console.log(red + ' ' + green + ' ' + blue);
+		return red + ' ' + green + ' ' + blue;
 	});
-
-console.log(foundColor.rgb);
-}
