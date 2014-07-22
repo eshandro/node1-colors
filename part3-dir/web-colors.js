6ecd8a865caab617ff47cc4a6f5449a1d2551bcb
Erik Shandrowski <erik843@gmail.com>
2014-07-21 18:18:12 -0600

    Bonus 2 - needs fix

--- part3-dir/web-colors.js
+++ part3-dir/web-colors.js
@@ -5,13 +5,12 @@ var colorname = process.argv[2];
 var findRGB = function() {
 	var colorname = process.argv[2];
 	var foundColor = _.find(
-		request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', 
-			function(error, response, body) {
-				console.log(JSON.parse(body));
-			}),
-		function(item) {
-			return item.name === colorname;
-		});
+	request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function(error, response, body) {
+		console.log(JSON.parse(body));
+	}),
+	function(item) {
+		return item.name === colorname;
+	});
 
 console.log(foundColor.rgb);
 }
